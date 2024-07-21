70948c46fb2fa773af9c35011306ab486cd9a7f4
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-07-21 11:42:20 +0300

    chore:added logo

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,9 +1,22 @@
+"use client";
 import { LoginForm } from "@/layout/auth/login";
+import Image from "next/image";
+import logo from "/public/logo-blue.png";
+import darklogo from "/public/logo-white.png";
+import { useTheme } from "next-themes";
 
 export default function LoginIn() {
+  const { theme } = useTheme();
+
+  // Determine which logo to use based on the current theme
+  const currentLogo = theme === "dark" ? darklogo : logo;
+
   return (
     <main className="relative flex min-h-screen flex-col items-center justify-center px-4 w-full">
-      <div className="absolute inset-0 -z-10 h-full w-full  bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)]  dark:bg-[linear-gradient(to_right,#f0f0f0_0.2px,transparent_0.2px),linear-gradient(to_bottom,#f0f0f0_0.2px,transparent_0.2px)] bg-[size:6rem_4rem]"></div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,#f0f0f0_0.2px,transparent_0.2px),linear-gradient(to_bottom,#f0f0f0_0.2px,transparent_0.2px)] bg-[size:6rem_4rem]"></div>
+      <div className="mb-6">
+        <Image src={currentLogo} alt="Logo" width={150} height={50} />
+      </div>
       <LoginForm />
     </main>
   );
