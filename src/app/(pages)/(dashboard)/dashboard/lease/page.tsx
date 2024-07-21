75b1cc9b235a3dba5ad3ed84e67eed0e467db401
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-07-21 12:24:14 +0300

    feat:made the routes layout

--- src/app/(pages)/(dashboard)/dashboard/lease/page.tsx
+++ src/app/(pages)/(dashboard)/dashboard/lease/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <aside>
-      <h1 className="text-4xl w-full px-8 font-bold">Dashboard</h1>
+      <h1 className="text-4xl w-full px-8 font-bold">Lease</h1>
     </aside>
   );
 }
---
+++ src/layout/nav/side-bar.tsx
@@ -0,0 +1,90 @@
+"use client";
+import Link from "next/link";
+import { Bell } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import sidebarItems from "@/util/objects/side-bar";
+import Image from "next/image";
+import logo from "/public/logo-blue.png";
+import darklogo from "/public/logo-white.png";
+import { useTheme } from "next-themes";
+import { usePathname } from "next/navigation"; // Import usePathname
+
+export function Sidebar() {
+  const { theme } = useTheme();
+  const pathname = usePathname(); // Get the current pathname
+
+  // Determine which logo to use based on the current theme
+  const currentLogo = theme === "dark" ? darklogo : logo;
+
+  return (
+    <div className="hidden border-r max-w-xs bg-muted/40 md:block">
+      <div className="flex h-full max-h-[90vh] flex-col gap-2">
+        <div className="flex h-14 gap-4 items-center border-b px-4 lg:h-[60px] lg:px-6">
+          <Link href="/" className="flex items-center gap-2 font-semibold">
+            <Image src={currentLogo} alt="Logo" width={60} height={60} />
+            <h1 className="text-sm font-bold">Lease Calculator</h1>
+          </Link>
+          <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+            <Bell className="h-4 w-4" />
+            <span className="sr-only">Toggle notifications</span>
+          </Button>
+        </div>
+        <div className="flex  flex-col justify-between items-start h-full">
+          <nav className="grid items-start px-2 text-sm font-medium lg:px-4 w-full">
+            {sidebarItems.map(
+              (
+                item: {
+                  href: string;
+                  label: string;
+                  icon: any;
+                  badge?: number;
+                },
+                index
+              ) => {
+                const IconComponent = item.icon; // Assign the icon component
+
+                // Check if the current pathname matches the item's href
+                const isActive = pathname === item.href;
+
+                return (
+                  <Link
+                    key={index}
+                    href={item.href}
+                    className={`flex items-center gap-3 rounded-lg px-3 py-4 transition-all ${
+                      isActive
+                        ? "dark:bg-muted dark:text-primary bg-primary text-background"
+                        : "text-muted-foreground hover:text-primary"
+                    }`}
+                  >
+                    <IconComponent className="h-4 w-4" />
+                    {item.label}
+                    {item.badge && (
+                      <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                        {item.badge}
+                      </Badge>
+                    )}
+                  </Link>
+                );
+              }
+            )}
+          </nav>
+          <div className="grid items-start px-2 text-sm font-medium lg:px-4 w-full">
+            {/* Check if the current pathname matches the settings href */}
+            <Link
+              href="/dashboard/setting"
+              className={`flex items-center gap-3 rounded-lg px-3 py-4 transition-all ${
+                pathname === "/dashboard/setting"
+                  ? "dark:bg-muted dark:text-primary bg-primary text-background"
+                  : "text-muted-foreground hover:text-primary"
+              }`}
+            >
+              <Bell className="h-4 w-4" />
+              Settings
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
