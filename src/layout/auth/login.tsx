8f37f07693789647b09da23e07f06689e1ad8630
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-07-21 11:24:26 +0300

    feat:added the signup form

--- src/layout/auth/login.tsx
+++ src/layout/auth/login.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { Button } from "@/components/ui/button";
+
 import {
   Card,
   CardContent,
@@ -9,8 +11,15 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
 
 export function LoginForm() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible((prevState) => !prevState);
+  };
   return (
     <Card className="w-full md:max-w-sm">
       <CardHeader>
@@ -24,9 +33,20 @@ export function LoginForm() {
           <Label htmlFor="email">Email</Label>
           <Input id="email" type="email" placeholder="m@example.com" required />
         </div>
-        <div className="grid gap-2">
+        <div className="grid gap-2 relative">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" required />
+          <Input id="password" type={passwordVisible ? "text" : "password"} />
+          <button
+            type="button"
+            className="absolute inset-y-0 right-0 top-6  px-3 flex items-center"
+            onClick={togglePasswordVisibility}
+          >
+            {passwordVisible ? (
+              <EyeOff className="w-5 h-5" />
+            ) : (
+              <Eye className="w-5 h-5" />
+            )}
+          </button>
         </div>
       </CardContent>
       <CardFooter>
---
+++ src/layout/auth/sign-up.tsx
@@ -0,0 +1,80 @@
+"use client";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
+const SignUpForm = () => {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible((prevState) => !prevState);
+  };
+  return (
+    <Card className="w-full md:max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-xl">Sign Up</CardTitle>
+        <CardDescription>
+          Enter your information to create an account
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="grid gap-4">
+          <div className="grid grid-cols-2 gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="first-name">First name</Label>
+              <Input id="first-name" placeholder="Max" required />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="last-name">Last name</Label>
+              <Input id="last-name" placeholder="Robinson" required />
+            </div>
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2 relative">
+            <Label htmlFor="password">Password</Label>
+            <Input id="password" type={passwordVisible ? "text" : "password"} />
+            <button
+              type="button"
+              className="absolute inset-y-0 right-0 top-6 px-3 flex items-center"
+              onClick={togglePasswordVisibility}
+            >
+              {passwordVisible ? (
+                <EyeOff className="w-5 h-5" />
+              ) : (
+                <Eye className="w-5 h-5" />
+              )}
+            </button>
+          </div>
+          <Button type="submit" className="w-full">
+            Create an account
+          </Button>
+        </div>
+        <div className="mt-4 text-center text-sm">
+          Already have an account?{" "}
+          <Link href="#" className="underline">
+            Sign in
+          </Link>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+export default SignUpForm;
