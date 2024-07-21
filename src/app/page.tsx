185485ef34aef72ad857babe563aa2f286d9839f
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-07-21 09:51:54 +0300

    chore: corrected some linting issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import { ModeToggle } from "@/components/custom/toogle/theme";
 import { LoginForm } from "@/layout/auth/login";
-import Image from "next/image";
 
 export default function Home() {
   return (
