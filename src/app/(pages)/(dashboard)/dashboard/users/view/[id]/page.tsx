7f13fc301946afce03e531c5d20364bc6ddfc834
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-07-22 01:56:40 +0300

    fix: added udefined data for edit and view user

--- src/app/(pages)/(dashboard)/dashboard/users/view/[id]/page.tsx
+++ src/app/(pages)/(dashboard)/dashboard/users/view/[id]/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
   return (
     <aside className="flex flex-col w-full gap-4 px-8 max-md:pt-32">
       <Breadcrumbs path="Dashboard/Users/View" />
-      <ViewUser />
+      <ViewUser userData={undefined} />
     </aside>
   );
 }
