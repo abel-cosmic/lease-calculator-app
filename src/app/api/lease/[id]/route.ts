ec1862461e61f48227a26699951efb6cf58e8467
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-07-23 01:17:24 +0300

    feat:added the delete lease and get by id

---
+++ src/app/api/lease/[id]/route.ts
@@ -0,0 +1,76 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth/next";
+import { options } from "@/lib/db/auth";
+
+const prisma = new PrismaClient();
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  try {
+    // Check the user's session
+    const session = await getServerSession(options);
+
+    if (!session) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const lease = await prisma.lease.findUnique({
+      where: { id },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!lease) {
+      return NextResponse.json({ error: "Lease not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(lease, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  try {
+    // Check the user's session
+    const session = await getServerSession(options);
+
+    if (!session) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    // Check if the lease exists
+    const lease = await prisma.lease.findUnique({
+      where: { id },
+    });
+
+    if (!lease) {
+      return NextResponse.json({ error: "Lease not found" }, { status: 404 });
+    }
+
+    // Delete the lease
+    await prisma.lease.delete({
+      where: { id },
+    });
+
+    return NextResponse.json(
+      { message: "Lease deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
