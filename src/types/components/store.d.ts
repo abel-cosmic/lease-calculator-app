9b014afaba356f71275914660ad39fea727309cd
Abel Shibabaw <abelshibabaw291@gmail.com>
2024-07-23 14:41:53 +0300

    fix:correct typ error of manage lease store

--- src/types/components/store.d.ts
+++ src/types/components/store.d.ts
@@ -1,3 +1,8 @@
 type UserStore = EntityStore<User[]>;
 type LeaseStore = EntityStore<Leases[]>;
-type ManageLeaseStore = EntityStore<ManageLeases[]>;
+type ManageLeaseStoreWithoutTimestamps = OmitFields<
+  ManageLeases,
+  "user" | "lease"
+>;
+
+type ManageLeaseStore = EntityStore<ManageLeaseStoreWithoutTimestamps[]>;
